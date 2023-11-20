416534db5acbabd3bcd1bb8e8b86423264dcee58
Ahmed Said <ahmed.said@jax.org>
2023-11-20 18:20:56 -0500

    actually fixed design aggregation function

--- scbl_utils/utils/defaults.py
+++ scbl_utils/utils/defaults.py
@@ -142,7 +142,7 @@ def n_cells_agg(n_cells_series: Series):
 def design_agg(designs: Series):
     overall_design = {}
     libs_with_design = designs.dropna()
-    for design in designs:
+    for design in libs_with_design:
         overall_design |= design
     return overall_design
 
