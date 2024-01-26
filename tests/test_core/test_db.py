ada69878f3d04dd1406f367d18e34a363b588a68
Ahmed Said <ahmed.said@jax.org>
2024-01-25 19:46:47 -0500

    added type: ignore to a couple lines to silence warnings

--- tests/test_core/test_db.py
+++ tests/test_core/test_db.py
@@ -67,7 +67,7 @@ def test_data_rows_to_db(
                     child, parent_tablename
                 )  # TODO: add a type-hint here?
                 correct_parent_id = joined_df.loc[
-                    joined_df[f'{child_tablename}.id'] == child.id, parent_id_col
+                    joined_df[f'{child_tablename}.id'] == child.id, parent_id_col  # type: ignore
                 ]
 
                 if not isinstance(correct_parent_id, pd.Series):
@@ -76,51 +76,10 @@ def test_data_rows_to_db(
                     correct_parent_id = correct_parent_id.values[0]
                 else:
                     raise ValueError(
-                        f'Duplicate children found for {child_tablename} with id {child.id}'
+                        f'Duplicate children found for {child_tablename} with id {child.id}'  # type: ignore
                     )
 
                 if assigned_parent is None:
                     assert pd.isna(correct_parent_id)
                 else:
                     assert assigned_parent.id == correct_parent_id
-
-
-# class TestMatchingRowsFromTable:
-#     """
-#     Tests for the `matching_rows_from_table` function.
-#     """
-
-#     def test_matching_rows_from_table(
-#         self,
-#         test_db_session: sessionmaker[Session],
-#         complete_db_objects: dict[str, Base],
-#     ):
-#         """
-#         Test that `matching_rows_from_table` returns the correct rows.
-#         This doesn't test every table, which may be a future TODO.
-#         """
-#         # Add the institution to the database
-#         with test_db_session.begin() as session:
-#             session.add(complete_db_objects['institution'])
-
-#         # Test that the function returns the correct (and only) institution
-#         with test_db_session.begin() as session:
-#             matched_institutions = get_matching_rows(
-#                 session,
-#                 {Institution.short_name: 'institution_short_name'},
-#                 data=[{'institution_short_name': 'JAX-GM'}],
-#                 data_filename='test.csv',
-#             )
-#             db_institution = session.execute(select(Institution)).scalar()
-
-#             assert matched_institutions[0] == db_institution
-
-#         # Test that given wrong information, the function raises
-#         with test_db_session.begin() as session:
-#             with raises(Abort):
-#                 get_matching_rows(
-#                     session,
-#                     {Institution.short_name: 'institution_short_name'},
-#                     data=[{'institution_short_name': 'wrong_value'}],
-#                     data_filename='test.csv',
-#                 )
