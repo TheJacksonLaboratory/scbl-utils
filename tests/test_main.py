dcaf74fb290c7cfef79063ace11ec67ce5d3a838
Ahmed Said <ahmed.said@jax.org>
2024-04-17 14:16:57 -0400

    add tests for main

--- tests/test_main.py
+++ tests/test_main.py
@@ -12,6 +12,10 @@ class TestSCBLUtils:
     def cli(self, config_dir: Path) -> SCBLUtils:
         return SCBLUtils(config_dir=config_dir)
 
+    @fixture
+    def data_dir(self) -> Path:
+        return Path(__file__).parent / 'data'
+
     def test_db_session(self, cli: SCBLUtils):
         with cli._db_sessionmaker.begin() as session:
             institution = Institution(
@@ -24,8 +28,17 @@ class TestSCBLUtils:
 
             assert len(institutions_in_db) == 1
 
-    def test_directory_to_db_correct_n_rows(self, cli: SCBLUtils):
-        data_dir = Path(__file__).parent / 'data'
+    def test_directory_to_db_correct_n_rows(self, cli: SCBLUtils, data_dir: Path):
+        cli._directory_to_db(data_dir)
+
+        with cli._db_sessionmaker.begin() as session:
+            institutions_in_db = session.execute(select(Institution)).scalars().all()
+            assert len(institutions_in_db) == 1
+
+    def test_redundant_directory_to_db_correct_n_rows(
+        self, cli: SCBLUtils, data_dir: Path
+    ):
+        cli._directory_to_db(data_dir)
         cli._directory_to_db(data_dir)
 
         with cli._db_sessionmaker.begin() as session:
@@ -39,8 +52,23 @@ class TestSCBLUtils:
             institutions_in_db = session.execute(select(Institution)).scalars().all()
             assert len(institutions_in_db) == 1
 
-    def test_fill_db_correct_n_rows(self, cli: SCBLUtils):
-        data_dir = Path(__file__).parent / 'data'
+    def test_redundant_gdrive_to_db_correct_n_rows(self, cli: SCBLUtils):
+        cli._gdrive_to_db()
+        cli._gdrive_to_db()
+
+        with cli._db_sessionmaker.begin() as session:
+            institutions_in_db = session.execute(select(Institution)).scalars().all()
+            assert len(institutions_in_db) == 1
+
+    def test_fill_db_correct_n_rows(self, cli: SCBLUtils, data_dir: Path):
+        cli.fill_db(data_dir)
+
+        with cli._db_sessionmaker.begin() as session:
+            institutions_in_db = session.execute(select(Institution)).scalars().all()
+            assert len(institutions_in_db) == 2
+
+    def test_redundant_fill_db_correct_n_rows(self, cli: SCBLUtils, data_dir: Path):
+        cli.fill_db(data_dir)
         cli.fill_db(data_dir)
 
         with cli._db_sessionmaker.begin() as session:
